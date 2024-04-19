a99ac06ec422b506ca63ac85a94c025c8ebc411f
MRxwr <nasserhatab@gmail.com>
2024-04-19 20:58:39 +0300

    Update checksouthead.php

--- admin/includes/checksouthead.php
+++ admin/includes/checksouthead.php
@@ -1,42 +1,45 @@
 <?php
+session_start();
+function selectDB($table, $where){
+    GLOBAL $dbconnect;
+    $check = [';', '"'];
+    $where = str_replace($check, "", $where);
+    $sql = "SELECT * FROM `{$table}`";
+    if (!empty($where)) {
+        $sql .= " WHERE {$where}";
+    }
+    if ($stmt = $dbconnect->prepare($sql)) {
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $array = array();
+        while ($row = $result->fetch_assoc()) {
+            $array[] = $row;
+        }
+        if (isset($array) && is_array($array)) {
+            return $array;
+        } else {
+            return 0;
+        }
+    } else {
+        $error = array("msg" => "select table error");
+		return 0;
+    }
+}
+
 if ( isset($_COOKIE["CreateKWUALFARAJ"]) && !empty($_COOKIE["CreateKWUALFARAJ"]) ){
-	session_start ();	
 	$svdva = $_COOKIE["CreateKWUALFARAJ"];
-	$sql = "SELECT * 
-			FROM `adminstration` 
-			WHERE `keepMeAlive` LIKE ?";
-	$stmt = $dbconnect->prepare($sql);
-	$svdvaParam = '%' . $svdva . '%';
-	$stmt->bind_param("s", $svdvaParam);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	// Check if there is one row returned
-	if ($result->num_rows == 1) {
-		$row = $result->fetch_assoc();
-		$userID = $row["id"];
-		$email = $row["email"];
-		$username = $row["fullName"];
+	if ( $user = selectDB("adminstration","`keepMeAlive` LIKE '%{$svdva}%'") ) {
+		$userID = $user[0]["id"];
+		$email = $user[0]["email"];
+		$username = $user[0]["fullName"];
+		$_SESSION['CreateKWUALFARAJ'] = $email;	
+	}elseif( $user = selectDB("employees","`keepMeAlive` LIKE '%{$svdva}%'") ){
+		$userID = $user[0]["id"];
+		$email = $user[0]["email"];
+		$username = $user[0]["fullName"];
 		$_SESSION['CreateKWUALFARAJ'] = $email;	
 	}else{
-		$sql = "SELECT * 
-				FROM `employees` 
-				WHERE `keepMeAlive` LIKE ?";
-		$stmt = $dbconnect->prepare($sql);
-		$svdvaParam = '%' . $svdva . '%';
-		$stmt->bind_param("s", $svdvaParam);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		if ( $result->num_rows == 1 ){
-			$row = $result->fetch_assoc();
-			$userID = $row["id"];
-			$email = $row["email"];
-			$username = $row["fullName"];
-			$_SESSION['CreateKWUALFARAJ'] = $email;	
-		}else{
-			header("Location: logout.php");die();
-		}
+		header("Location: logout.php");die();
 	}
-}else{
-	header("Location: login.php");die();
 }
 ?>
