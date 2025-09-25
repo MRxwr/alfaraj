b6ba79418fd82e70f3bf919e2c81939694311a86
MRxwr <nasserhatab@gmail.com>
2025-09-26 02:22:27 +0300

    save

--- admin/includes/functions/payment.php
+++ admin/includes/functions/payment.php
@@ -396,7 +396,6 @@ for ($i = 0; $i < 3; $i++) {
 				}
 			}else{
 				unset($paymentDetails["paymentMethod"]);
-				die("am here");
 				if( insertDB("bookInvoices", $paymentDetails) ){
 					return $paymentURL;die();
 				}else{
